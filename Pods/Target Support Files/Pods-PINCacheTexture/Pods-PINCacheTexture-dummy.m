88b3cf1614291a5e82495a1cffa6b0a3852dfaab
Yogesh Kumar <yogesh.kumar1@timesinternet.in>
2019-01-16 09:17:54 +0530

    Replaced Carthage with CocoaPods

---
+++ Pods/Target Support Files/Pods-PINCacheTexture/Pods-PINCacheTexture-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PINCacheTexture : NSObject
+@end
+@implementation PodsDummy_Pods_PINCacheTexture
+@end
---
+++ Pods/Target Support Files/Pods-PINCacheTextureTests/Pods-PINCacheTextureTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PINCacheTextureTests : NSObject
+@end
+@implementation PodsDummy_Pods_PINCacheTextureTests
+@end
